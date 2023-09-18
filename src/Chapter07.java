6cbb2d7bb89a128b7a7e21759813f098ae394b7a
HierarchThurs <hierarchthurs@gmail.com>
2023-09-18 09:05:10 +0800

    Create Chapter07

---
+++ src/Chapter07.java
@@ -0,0 +1,16 @@
+/**
+ * {@code @Description}
+ * {@code @author}          Hierarch
+ * {@code @file}            Chapter07
+ * {@code @project}         HirTusJava
+ * {@code @software}        IntelliJ IDEA
+ * {@code @create}          2023-09-01 9:50
+ */
+public class Chapter07 {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg + "、");
+        }
+        System.out.println("Hello");
+    }
+}
